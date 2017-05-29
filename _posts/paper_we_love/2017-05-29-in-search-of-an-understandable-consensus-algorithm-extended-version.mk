3a727912026f316b84f869261d9a3ce12115dc51
zhangchaowei <feelkill2009@gmail.com>
2017-05-29 21:25:56 +0800

    new raft file

---
+++ _posts/paper_we_love/2017-05-29-in-search-of-an-understandable-consensus-algorithm-extended-version.mk
@@ -0,0 +1,12 @@
+---
+layout: post
+title: "寻找一种易于理解的一致性算法（扩展版）"
+date: 2017-05-29
+category: 论文
+keywords: Raft
+---
+
+论文题目: [In Search of an Understandable Consensus Algorithm](/pieces_of_work/concurrent_control/raft.pdf)
+
+todo
+
